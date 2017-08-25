5793588b7751753b4b63214e7345e325ea21e548
J. Famous <joshua.famous@gmail.com>
2017-08-25 00:34:07 -0400

    Revert config

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 //define database name as resumix - for use with local MySQL database (currently XAMPP)
-define('DB_NAME', 'chapters_chaptersweet');
+define('DB_NAME', 'chaptersweet');
 //access via root user
 //TODO - make a seperate user for this program
-define('DB_USER', 'chapters_root');
+define('DB_USER', 'root');
 //password (super secret)
 //~~~NOTE TO SELF~~~ change this before uploading to github
 //dont want any
@@ -11,7 +11,7 @@ define('DB_USER', 'chapters_root');
 //  ~~~~~~~~~~~~~~HACKER~~~~~~~
 //  ~~~~PEOPLE~~~~~~~~~~~~~~~~~
 //seeing this
-define('DB_PASSWORD', 'BLANK');
+define('DB_PASSWORD', '');
 //define host as localhost, connecting to own machine
 define('DB_HOST', 'localhost');
 //charset is utf8, latin
